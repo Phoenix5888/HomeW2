39ec3df1f35c880e8d842ab9de13c27ec0a11efe
Hanna Liubarska <gvg8say@gmail.com>
2024-10-07 13:31:52 +0300

    bug fix

--- mail.js
+++ mail.js
@@ -1,17 +1,18 @@
 undefined + 1;			//  NaN			|| number
 'true' == true			//  false		|| boolean
 false == 'false'		//  false		|| boolean
-null == ''			//  false		|| boolean
+null == ''		     	//  false	    || boolean
 3 ** (9 / 3);			//    1			|| number	
-!!'false' == !!'true'		//  true		|| boolean
+!!'false' == !!'true'	//  true	    || boolean
 0 || '0' && 1			//    1			|| number
-1 < 2 < 3			//  true		|| boolean
+1 < 2 < 3			//  true		    || boolean
 'foo'+ + +'bar'			// 'fooNaN'або 'NaNbar'	|| string (в залежності від оператору він в постфіксній формі чи префікс формі)
-3 ** 2 / 3;			//    0			|| number
-1 < 2 > 3			// false		|| boolean
-(+null == false) < 1;		// true			|| boolean
-false && true || true		// true			|| boolean
-false && (true || true);	// false		|| boolean
-(+null == false) < 1 ** 5;	// true                 || boolean
+3 ** 2 / 3;			//    3			    || number
+1 < 2 > 3			// false		    || boolean
+(+null == false) < 1;		// true		|| boolean
+false && true || true		// true		|| boolean
+false && (true || true);	// false	|| boolean
+(+null == false) < 1 ** 5;	// true     || boolean
+
 
 
